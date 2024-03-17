524e06e0b050238bdff7b2a0150ffed04344f563
QPP123 <andrew.qiaokkuiyao@gmail.com>
2024-03-17 04:52:14 -0400

    download_data.R update

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -5,14 +5,18 @@
 # Contact: kuiyao.qiao@mail.utoronto.ca
 # License: MIT
 
-# The download of the raw data requires filtering and browsing The General Social Survey (https://gss.norc.org/documents/stata/GSS_stata.zip). 
-# The whole replication package is available on-site for downloading regarding to the GSS Codebook.
-
-#### Workspace setup ####
-#library(opendatatoronto)
-#library(tidyverse)
-
 #### Download data ####
+#Access to GSS Data
+#Step 1: Visit the GSS website
+#Visit the GSS website: https://gssdataexplorer.norc.org/home
 
+#Step 2: Register for Access
+#Register for an account by clicking on the orange button "CREATE AN ACCOUNT" on the home page and following the prompts.
+
+#Step 3: Find relevant datasets
+#You can then use the search function on the website to complete the data collection.
 
 #### Save data ####
+
+#Step 4: Download the data
+#Once you have found the relevant dataset, you can start downloading it. The download method and instructions can be obtained from the URL "https://gss.norc.org/get-the-data".
